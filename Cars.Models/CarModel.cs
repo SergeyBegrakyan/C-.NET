586bf4dbe203e1a78d2199f0d76411bc5f4611e8
SergeyBegrakyan <92976950+SergeyBegrakyan@users.noreply.github.com>
2024-05-14 00:15:26 +0400

    Add files via upload

---
+++ Cars.Models/CarModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Models
+{
+    public class CarModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public DateTime Year { get; set; }
+    }
+}
---
+++ Cars.Models/CarsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cars.Models
+{
+    public class CarsModel
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public DateTime Year { get; set; }
+    }
+}
