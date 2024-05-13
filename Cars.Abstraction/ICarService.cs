21893d8847385e3fcb5e33626db4be0c7b1809ec
SergeyBegrakyan <92976950+SergeyBegrakyan@users.noreply.github.com>
2024-05-14 00:14:42 +0400

    Add files via upload

---
+++ Cars.Abstraction/ICarService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using Cars.Models;
+
+namespace Cars.Abstraction
+{
+    public interface ICarService
+    {
+        List<CarModel> GetAllCars();
+        List<CarModel> GetAllCars(string searchTerm);
+        CarModel GetCarById(int id);
+        void UpdateCar(CarModel updatedCar);
+
+     
+        //List<CarsModel> GetAllCars();
+        //List<CarsModel> GetCars(string searchTerm);
+        void AddCar(CarModel car);
+        //void UpdateCar(CarsModel car);
+        void DeleteCar(int id);
+    }
+}
